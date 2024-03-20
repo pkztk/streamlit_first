c7ed3b38d84c648cdd06182c44e253dbb27d1ce1
pkztk <pkoziatek@gmail.com>
2024-03-20 14:07:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import streamlit as st
 st.title('Predict Transaction')
 st.markdown('---')
 
-st.title('Fill Your Transaction Data')
+ Account = st.text_input('Sender Account',value='80E40A2C0', help='Account Code')
